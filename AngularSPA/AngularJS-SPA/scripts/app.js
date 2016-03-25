2a3952ae526bfc34caa055f510d72490f8863be0
kamranmyaseen <kamranmyaseen@gmail.com>
2016-03-26 02:59:20 +0300

    Crud operation in AngularJS and requireJS with Web Api
    Crud operation in AngularJS and requireJS with Web Api

---
+++ AngularSPA/AngularJS-SPA/scripts/app.js
@@ -0,0 +1,35 @@
+﻿define(['ui-router', 'ngStorage', 'ngCookies'], function () {
+
+    //defining angularjs module
+    var app = angular.module("app", ['ui.router', 'ngCookies', 'ngStorage']);
+
+    //global service
+    app.constant("utility",
+        {
+            baseAddress: "http://localhost:5100/api/"
+        });
+
+    //manual bootstrap
+    app.init = function () {
+        angular.bootstrap(document, ['app']);
+    };
+
+    //defining routes
+    app.config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state("home", {
+                url: "/",
+                templateUrl: 'views/home/home.html',
+                controller: 'homeCtrl'
+            })
+        .state("about", {
+            url: "/about",
+            templateUrl: 'views/account/about.html',
+            controller: 'aboutCtrl'
+        });
+    });
+    return app;
+});
---
+++ AngularSPA/AngularJS-SPA/scripts/main.js
@@ -0,0 +1,50 @@
+﻿require.config({
+    baseurl: '/scripts/',
+    paths: {
+        'angular': 'libs/angular',
+        'ngStorage': 'libs/ngStorage',
+        'ngCookies': 'libs/angular-cookies',
+        'ui-router': 'libs/angular-ui-router',
+        'jquery': 'libs/jquery-1.10.2',
+        'bootstrap': 'libs/bootstrap',
+        'service': 'services/service',
+        'homeCtrl': "controllers/homeCtrl",
+        'accountCtrl': "controllers/accountCtrl",
+        'filter': "filters/filter",
+    },
+    shim: {
+        ngStorage: {
+            deps: ['angular'],
+            exports: 'angular'
+        },
+        ngCookies: {
+            deps: ['angular'],
+            exports: 'angular'
+        },
+        'ui-router': {
+            deps: ['angular'],
+            exports: 'angular'
+        },
+        angular: {
+            exports: 'angular'
+        },
+        bootstrap: {
+            deps: ['jquery']
+        }
+    },
+    deps: ['app']
+});
+
+require([
+    "app",
+    "filter",
+    "bootstrap",
+    "homeCtrl",
+    "accountCtrl"
+],
+
+    function (app) {
+        //bootstrapping app
+        app.init();
+    }
+);
